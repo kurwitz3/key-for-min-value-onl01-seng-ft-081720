b3f89815c11266bed100bce42222d7493561f7c4
Richard Kurwitz <kurwitz3@aol.com>
2020-08-20 21:53:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   lowest_value  = nil 
   lowest_key = nil 
-  name_hash.collect  do |key,value|
+  name_hash.each  do |key,value|
      if value < lowest_value 
     lowest_value = value
     lowest_key = key
